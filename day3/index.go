5c13e34049832ff5d9da0d09074e51c990f436ee
ariesdimasy <ariesdimasy@gmail.com>
2021-12-13 10:38:11 +0700

    update muncul sekali

--- day3/index.go
+++ day3/index.go
@@ -21,13 +21,25 @@ func main() {
 	// fmt.Println(src.Pow(5, 3))  // 125
 
 	// Test cases
-	fmt.Println(src.JoinArrayRemoveDuplicate([]string{"apel", "anggur"}, []string{"lemon", "leci", "nanas"}))
-	// ["apel", "anggur", "lemon", "leci", "nanas"]
+	// fmt.Println(src.JoinArrayRemoveDuplicate([]string{"apel", "anggur"}, []string{"lemon", "leci", "nanas"}))
+	// // ["apel", "anggur", "lemon", "leci", "nanas"]
 
-	fmt.Println(src.JoinArrayRemoveDuplicate([]string{"samsung", "apple"}, []string{"apple", "sony", "xiaomi"}))
-	// ["samsung", "apple", "sony", "xiaomi"]
+	// fmt.Println(src.JoinArrayRemoveDuplicate([]string{"samsung", "apple"}, []string{"apple", "sony", "xiaomi"}))
+	// // ["samsung", "apple", "sony", "xiaomi"]
 
-	fmt.Println(src.JoinArrayRemoveDuplicate([]string{"football", "basketball"}, []string{"basketball", "football"}))
-	// [football basketball]
+	// fmt.Println(src.JoinArrayRemoveDuplicate([]string{"football", "basketball"}, []string{"basketball", "football"}))
+	// // [football basketball]
+
+	// fmt.Println(src.MunculSekali("1234123"))    // [4]
+	// fmt.Println(src.MunculSekali("76523752"))   // [6, 3]
+	// fmt.Println(src.MunculSekali("12345"))      // [1 2 3 4 5]
+	// fmt.Println(src.MunculSekali("1122334455")) // []
+	// fmt.Println(src.MunculSekali("0872504"))    // [8 7 2 5 4]
+
+	fmt.Println(src.PairSum([]int{1, 2, 3, 4, 6}, 6)) // [1, 3]
+	fmt.Println(src.PairSum([]int{2, 5, 9, 11}, 11))  // [0, 2]
+	fmt.Println(src.PairSum([]int{1, 3, 5, 7}, 12))   // [2, 3]
+	fmt.Println(src.PairSum([]int{1, 4, 6, 8}, 10))   // [1, 2]
+	fmt.Println(src.PairSum([]int{1, 5, 6, 7}, 6))    // [0, 1]
 
 }
--- day3/src/challenge3.go
+++ day3/src/challenge3.go
@@ -2,6 +2,7 @@ package src
 
 import (
 	"math"
+	"strconv"
 )
 
 func PrimeNumber(number int) bool {
@@ -44,11 +45,54 @@ func Pow(x, n int) int {
 func JoinArrayRemoveDuplicate(arrayA, arrayB []string) []string {
 
 	var newArr = []string{}
+	var combine = append(arrayA, arrayB...)
 
-	for i := 0; i < len(arrayA); i++ {
+	for i := 0; i < len(combine); i++ {
+		var exists bool = false
+		for j := 0; j < len(newArr); j++ {
+			if combine[i] == newArr[j] {
+				exists = true
+				break
+			}
+		}
 
+		if !exists {
+			newArr = append(newArr, combine[i])
+		}
 	}
 
 	// your code here
-	return []string{"", ""}
+	return newArr
+}
+
+func MunculSekali(angka string) []int {
+	// your code here
+	var result = []int{}
+
+	for i := 0; i < len(angka); i++ {
+		var misuketa bool = false
+		for j := 0; j < len(angka); j++ {
+			//fmt.Println(string(angka[i]), string(angka[j]), "\t")
+			if string(angka[i]) == string(angka[j]) && i != j {
+				//fmt.Println("MISUKETAZOO")
+				misuketa = true
+				break
+			}
+		}
+
+		if !misuketa {
+			strAngka := string(angka[i])
+			newNum, err := strconv.ParseInt(strAngka, 10, 32)
+			if err != nil {
+				panic(err)
+			}
+			result = append(result, int(newNum))
+		}
+	}
+
+	return result
+}
+
+func PairSum(arr []int, target int) []int {
+
 }
